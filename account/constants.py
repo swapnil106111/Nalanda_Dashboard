24165386b6138bfce82e21eb0c11f210144690d0
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2017-09-12 16:36:42 +0530

    Added a new file for constants

---
+++ account/constants.py
@@ -0,0 +1,8 @@
+metrics = [
+	{'displayName': '% exercise completed', 'toolTip': ''},
+	{'displayName': '% exercise correct', 'toolTip': ''},
+	{'displayName': '# attempts completed', 'toolTip': ''},
+	{'displayName': '% students completed the topic', 'toolTip': ''},
+]
+
+USERROLE = { 'BM':1, 'SL':2, 'teacher':3 }
