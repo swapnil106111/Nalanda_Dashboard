6a35a12ef1313dac6cbfdb5b51bd5e11c3913892
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2017-11-09 17:42:59 +0530

    commented code of % students completed the topic

--- account/constants.py
+++ account/constants.py
@@ -2,7 +2,7 @@ metrics = [
 	{'displayName': '% exercise completed', 'toolTip': ''},
 	{'displayName': '% exercise correct', 'toolTip': ''},
 	{'displayName': '# attempts completed', 'toolTip': ''},
-	{'displayName': '% students completed the topic', 'toolTip': ''},
+	# {'displayName': '% students completed the topic', 'toolTip': ''},
 ]
 
 USERROLE = { 'BM':1, 'SL':2, 'teacher':3 }
