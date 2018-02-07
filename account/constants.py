cdca39f41ab0af8b547e61fec73c35b73380bead
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-02-07 17:05:26 +0530

    Added new metrics column on UI

--- account/constants.py
+++ account/constants.py
@@ -7,12 +7,13 @@
 
 metrics = [	
 	{'displayName': '# Mastered topics', 'toolTip': ''},
-	{'displayName': '% of mastered topics', 'toolTip': ''},
-	{'displayName': '% exercise completed', 'toolTip': ''},
-	{'displayName': '% exercise correct', 'toolTip': ''},
+	{'displayName': '% Mastered topics', 'toolTip': ''},
 	{'displayName': '# attempts completed', 'toolTip': ''},
+	{'displayName': '% exercise correct', 'toolTip': ''},
+	{'displayName': '% exercise completed', 'toolTip': ''},
+	
 	#{'displayName': '% students completed the topic', 'toolTip': ''},
-	{'displayName': 'Sample metrics', 'toolTip': ''},  #  Added For Testing
+	# {'displayName': 'Sample metrics', 'toolTip': ''},  #  Added For Testing
 ]
 
 USERROLE = { 'BM':1, 'SL':2, 'teacher':3 }
