aada96af662d0d112335c352e04bf49cfa6dfce2
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-02-28 17:11:30 +0530

    Added constants for metrics

---
+++ usersession/constants.py
@@ -0,0 +1,5 @@
+metrics = [	
+	{'displayName': '# Total Active Usage ', 'toolTip': 'It shows user session data in HH:MM:SS'}
+	# {'displayName': '# Minutes', 'toolTip': ''},
+	# {'displayName': '# Seconds', 'toolTip': 'It shows the total number of Seconds'}
+]
