20731e1b3d1483e5b5aa64fae74890737b850e15
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-02-28 17:10:31 +0530

    Added urls in user session app to access role based user session details

--- usersession/urls.py
+++ usersession/urls.py
@@ -3,5 +3,6 @@ from usersession import views
 
 urlpatterns = [
 	url(r'^api/usersession/get-page-meta', views.get_page_meta_view, name='get_page_meta'),
-	url(r'^api/usersession/get-page-data', views.get_page_data_view, name='get_page_data')
+	url(r'^api/usersession/get-page-data', views.get_page_data_view, name='get_page_data'),
+	url(r'^api/usersession/trend', views.get_trend_data_view, name='get_trend_data')
 ]
