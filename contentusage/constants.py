8ca9cd75f9a6577ca877c55c7a012e557dce6432
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-03-23 12:35:41 +0530

    added constants to show metrics for content usage

---
+++ contentusage/constants.py
@@ -0,0 +1,6 @@
+METRICS = [	
+	{'displayName': '# Question attempts ', 'toolTip': ''},
+	{'displayName': '% Question completed ', 'toolTip': ''}
+]
+
+CHANNELDBLIST = ['8fc515793aad4e52b490b03c48ad21de','c55ea9d8afc7493caacfa8f23b7d385e','eb99f209f9c34ba192f6e695aeb37e4f','9e5305326ed742d0892479dea825a514','e890b1acbcad44939688dbb1b7dde2e5','5e3cea43f5e1411599132065b56f8934']
