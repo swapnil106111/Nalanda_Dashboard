8ad1a95087421bfada736c13af64216db0a8fce9
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-03-23 12:32:24 +0530

    Added views for content usage page meta and page data

--- contentusage/views.py
+++ contentusage/views.py
@@ -2,42 +2,64 @@ from django.shortcuts import render, HttpResponse
 from account.views import construct_response
 from account.models import UserInfoClass, UserInfoSchool, UserInfoStudent
 import json 
+from .contentusagereport import ContentUsageMeta, ContentUsageData, SchoolDetails
+from contentusage.constants import *
+
 # Create your views here.
 
 def get_schools(request):
-    """
-    This function gets all schools and classes in the database
-    Args:
-        None
-    Returns:
-        school_info(dict) = It contains schoolinfo and it's associated classes and student info
-    """
-    if request.method == 'POST':
-        totalschools = {}
-        school_list = []
-        schools = UserInfoSchool.objects.all()
-        def convert_to_string(data):
-            data['id'] = str(data['id'])
-            return data
-        # Get all the schools, if schools exist
-        for school in schools:
-            school_info = {}
-            classes_in_school = list(UserInfoClass.objects.filter(parent=school.school_id).extra(select={'id':'class_id','name':'class_name'}).values('id','name'))
-            for classid in classes_in_school:
-                students_in_class = list(UserInfoStudent.objects.filter(parent=classid['id']).extra(select={'id':'student_id','name':'student_name'}).values('id','name'))
-                classid['children'] = students_in_class
-            school_info['id'] = str(school.school_id)
-            school_info['name'] = school.school_name
-            school_info['children'] = list(map(convert_to_string, classes_in_school))
-            school_list.append(school_info)
-        totalschools["schools"] = school_list
-        response = construct_response(0, '', '', totalschools);
-        response_text = json.dumps(response,ensure_ascii=False)
-        return HttpResponse(response_text,content_type='application/json')
-    else:
-        response = construct_response(1111,'wrong request','wrong request','')
-        response_text = json.dumps(response,ensure_ascii=False)
-        return HttpResponse(response_text,content_type='application/json')
+	"""
+	This function gets all schools and classes in the database
+	Args:
+		None
+	Returns:
+		school_info(dict) = It contains schoolinfo and it's associated classes and student info
+	"""
+	if request.method == 'POST':
+		user = request.user
+		body_unicode = request.body.decode('utf-8')
+		data = json.loads(body_unicode)
+		parent_level = data.get('parentLevel', -2)
+		objSchoolDetails = SchoolDetails(user, parent_level)
+		objSchoolData = objSchoolDetails.getPageData()
+		response = construct_response(0, '', '', objSchoolData);
+		response_text = json.dumps(response,ensure_ascii=False)
+		return HttpResponse(response_text,content_type='application/json')
+	else:
+		response = construct_response(1111,'wrong request','wrong request','')
+		response_text = json.dumps(response,ensure_ascii=False)
+		return HttpResponse(response_text,content_type='application/json')
+
+def get_contentusage_page_meta(request):
+	user = request.user
+	body_unicode = request.body.decode('utf-8')
+	data = json.loads(body_unicode)
+	parent_level = data.get('parentLevel', -2)
+	# parent_id = int(data.get('parentId', '').strip())
+	objContentUsage= ContentUsageMeta(user, parent_level)
+	objContentUsageData = objContentUsage.getInstituteMeta(METRICS)
+	response_text = json.dumps(objContentUsageData,ensure_ascii=False)
+	return HttpResponse(response_text,content_type='application/json')
+
+def get_contentusage_page_data(request):
+
+	user = request.user
+	body_unicode = request.body.decode('utf-8')
+	data = json.loads(body_unicode)
+	print ("data:", data)
+	startTimestamp = data.get('startTimestamp', 0)
+	endTimestamp = data.get('endTimestamp', 0)
+	topicID = data.get('contentId', '')
+	parentLevel = data.get('parentLevel', -1)
+	parentID = int(data.get('parentId', '').strip())
+	channelID = data.get('channelId', '')
+	filtercriteria = data.get('std', '')
+
+	objContentUsage = ContentUsageData(user, parentID, parentLevel, topicID, channelID, startTimestamp, endTimestamp)
+	objContentUsageData = objContentUsage.getInstituteData(filtercriteria)
+	response_object = construct_response(0, "", "", objContentUsageData)
+	response_text = json.dumps(response_object,ensure_ascii=False)
+	return HttpResponse(response_text, content_type='application/json')
 
 def get_contentusage(request):
     if request.method == 'GET':
