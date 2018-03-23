63a0a5bbbb8223a03776fa403885a7451b80d6ce
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-03-23 12:29:54 +0530

    Added urls for content usage page meta and data

--- contentusage/urls.py
+++ contentusage/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
-
 from contentusage import views
 
 urlpatterns = [
     url(r'^content-usage/', views.get_contentusage, name='get_contentusage'),
     url(r'^api/contentusage/schools', views.get_schools, name='get_schools'),
+    url(r'^api/contentusage/get-page-meta', views.get_contentusage_page_meta, name='get_contentusage_page_meta'),
+    url(r'^api/contentusage/get-page-data', views.get_contentusage_page_data, name='get_contentusage_page_data'),
 ]
