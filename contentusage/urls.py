832daf977c29720f77fc575d03a4d72de4082d6d
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-04-06 16:36:14 +0530

    Added url to get trend details

--- contentusage/urls.py
+++ contentusage/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import include, url
 from contentusage import views
 
 urlpatterns = [
-    url(r'^content-usage/', views.get_contentusage, name='get_contentusage'),
-    url(r'^api/contentusage/schools', views.get_schools, name='get_schools'),
-    url(r'^api/contentusage/get-page-meta', views.get_contentusage_page_meta, name='get_contentusage_page_meta'),
-    url(r'^api/contentusage/get-page-data', views.get_contentusage_page_data, name='get_contentusage_page_data'),
+    url(r'^content-usage/', views.get_contentusage, name = 'get_contentusage'),
+    url(r'^api/contentusage/schools', views.get_schools, name = 'get_schools'),
+    url(r'^api/contentusage/get-page-meta', views.get_contentusage_page_meta, name = 'get_contentusage_page_meta'),
+    url(r'^api/contentusage/get-page-data', views.get_contentusage_page_data, name = 'get_contentusage_page_data'),
+    url(r'^api/contentusage/trend', views.get_trend_data, name = 'get_trend_data')
 ]
