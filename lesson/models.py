e874d6aefea2279abbebc5d10b0ad82159992a71
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-06-25 15:13:38 +0530

    Modified type of lesson_id as char

--- lesson/models.py
+++ lesson/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Lesson(models.Model):
-    lesson_id = models.BigIntegerField(primary_key=True)
+    lesson_id = models.CharField(primary_key=True, max_length=60)
     lesson_name = models.CharField(max_length=60)
     class_id = models.IntegerField()
     lesson_content = models.TextField(null=False, blank=False)
