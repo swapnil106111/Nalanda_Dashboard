f4f5be7b64aba68938a018dd80362bd75f62cbb2
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-06-28 18:22:13 +0530

    Added a url to fetch lesson mastery details

--- lesson/urls.py
+++ lesson/urls.py
@@ -3,5 +3,6 @@ from lesson import views
 
 urlpatterns = [
     url(r'^lesson-mastery/', views.get_lesson_mastery, name = 'get_lesson_mastery'),
-    url(r'^api/lesson/lessons', views.get_lessons, name ='get_lessons')
+    url(r'^api/lesson/lessons', views.get_lessons, name ='get_lessons'),
+    url(r'^api/lesson/get-page-data', views.get_lesson_page_data, name = 'get_lesson_page_data')
 ]
