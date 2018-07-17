5b5d37468cf9178e7aab31d17aa70329a1ecb4ca
yogeshmhaskule <yogesh.mhaskule@gmail.com>
2018-07-17 17:54:26 +0530

    Added log for lesson app

--- lesson/lessonmastery.py
+++ lesson/lessonmastery.py
@@ -7,6 +7,10 @@ import datetime
 import json, time
 from django.db.models import Count
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 class LessonDetails(BaseRoleAccess):
 	def __init__(self, user, parentLevel):
 		""" Used to fetch the school/class based on user role
@@ -29,23 +33,26 @@ class LessonDetails(BaseRoleAccess):
 		Returns:
 			totalscho(dict) : dict of schools with it's classes and students as a children
 		"""
-		school_list = []
-		schools = UserInfoSchool.objects.filter(school_id__in = self.institutes)
-		# Get all the schools, if schools exist
-		for school in schools:
-			school_info = {}
-			classes_in_school = list(UserInfoClass.objects.filter(parent=school.school_id).extra(select={'id':'class_id','name':'class_name'}).values('id','name'))
-			for classid in classes_in_school:
-				lessons_in_class = list(Lesson.objects.filter(class_id=classid['id']).extra(select={'id':'lesson_id','name':'lesson_name'}).values('id','name'))
-				for lesson in lessons_in_class:
-					lesson['id'] = str(lesson['id'])
-				classid['children'] = lessons_in_class
-			school_info['id'] = str(school.school_id)
-			school_info['name'] = school.school_name
-			school_info['children'] = list(map(self.convert_to_string, classes_in_school))
-			school_list.append(school_info)
-		self.totallessons["schools"] = school_list
-		return self.totallessons
+		try:
+			school_list = []
+			schools = UserInfoSchool.objects.filter(school_id__in = self.institutes)
+			# Get all the schools, if schools exist
+			for school in schools:
+				school_info = {}
+				classes_in_school = list(UserInfoClass.objects.filter(parent=school.school_id).extra(select={'id':'class_id','name':'class_name'}).values('id','name'))
+				for classid in classes_in_school:
+					lessons_in_class = list(Lesson.objects.filter(class_id=classid['id']).extra(select={'id':'lesson_id','name':'lesson_name'}).values('id','name'))
+					for lesson in lessons_in_class:
+						lesson['id'] = str(lesson['id'])
+					classid['children'] = lessons_in_class
+				school_info['id'] = str(school.school_id)
+				school_info['name'] = school.school_name
+				school_info['children'] = list(map(self.convert_to_string, classes_in_school))
+				school_list.append(school_info)
+			self.totallessons["schools"] = school_list
+			return self.totallessons
+		except Exception as e:
+			logger.error(e)
 
 	def getClassData(self):
 		""" Used to get class data for user 
@@ -54,20 +61,23 @@ class LessonDetails(BaseRoleAccess):
 		Returns:
 			totallessons(dict) : returns lesson details with class/schools 
 		"""
-		class_info = {}
-		class_list = []
-		classes = list(UserInfoClass.objects.filter(class_id__in = self.classes).extra(select={'id':'class_id','name':'class_name'}).values('id','name'))
-		for classid in classes:
+		try:
 			class_info = {}
-			lessons_in_class = list(Lesson.objects.filter(class_id=classid['id']).extra(select={'id':'lesson_id','name':'lesson_name'}).values('id','name'))
-			for lesson in lessons_in_class:
-					lesson['id'] = str(lesson['id'])
-			class_info['children'] = lessons_in_class
-			class_info['id'] = str(classid['id'])
-			class_info['name'] = classid['name']
-			class_list.append(class_info)
-		self.totallessons["schools"] = class_list
-		return self.totallessons
+			class_list = []
+			classes = list(UserInfoClass.objects.filter(class_id__in = self.classes).extra(select={'id':'class_id','name':'class_name'}).values('id','name'))
+			for classid in classes:
+				class_info = {}
+				lessons_in_class = list(Lesson.objects.filter(class_id=classid['id']).extra(select={'id':'lesson_id','name':'lesson_name'}).values('id','name'))
+				for lesson in lessons_in_class:
+						lesson['id'] = str(lesson['id'])
+				class_info['children'] = lessons_in_class
+				class_info['id'] = str(classid['id'])
+				class_info['name'] = classid['name']
+				class_list.append(class_info)
+			self.totallessons["schools"] = class_list
+			return self.totallessons
+		except Exception as e:
+			logger.error(e)
 
 	def getPageData(self):
 		result = self.parentLevelMethods[self.role]()
@@ -97,14 +107,17 @@ class LessonMastery(object):
 		Returns:
 			columns(list): It returns list of topic names
 		"""
-		columns = [{'name':'first', 'title':'Student Name'}, {'name': 'second','title': 'Metrics'}]
-		topic_dict = {}
-		topic_list = Content.objects.filter(topic_id__in=self.content_list).values_list('topic_name', flat=True)
-		for topic in topic_list:
-			topic_dict['title'] = topic
-			columns.append(topic_dict)
+		try:
+			columns = [{'name':'first', 'title':'Student Name'}, {'name': 'second','title': 'Metrics'}]
 			topic_dict = {}
-		return columns
+			topic_list = Content.objects.filter(topic_id__in=self.content_list).values_list('topic_name', flat=True)
+			for topic in topic_list:
+				topic_dict['title'] = topic
+				columns.append(topic_dict)
+				topic_dict = {}
+			return columns
+		except Exception as e:
+			logger.error(e)
 
 	def get_lesson_mastery_results(self):
 		""" It's used to fetch the mastry metrics result of respective content_id used in lesson
@@ -113,34 +126,40 @@ class LessonMastery(object):
 		Returns:
 			student_res_list(list):It returns student list includingg metrics result
 		"""
-		student_res_list = []
-		i_student = []
-		filter_lessons= {}
-		filter_lessons['date__range'] = (self.startTimestamp, self.endTimestamp)
-		filter_lessons['content_id__in'] = self.content_list
-		student_list = UserInfoStudent.objects.filter(parent = self.class_id).values('student_id', 'student_name')
-		for student in student_list:
-			for metrics in self.metrics_list:
-				res = list(MasteryLevelStudent.objects.filter(student_id = student['student_id']).filter(**filter_lessons).values_list(metrics,flat=True))
-				res = self.set_default_metrics_data(res)
-				i_student.append(student['student_name'])
-				i_student.append(metrics)
-				if res:
-					i_student.extend(res)	
-				student_res_list.append(i_student)
-				i_student = []
-		return student_res_list
+		try:
+			student_res_list = []
+			i_student = []
+			filter_lessons= {}
+			filter_lessons['date__range'] = (self.startTimestamp, self.endTimestamp)
+			filter_lessons['content_id__in'] = self.content_list
+			student_list = UserInfoStudent.objects.filter(parent = self.class_id).values('student_id', 'student_name')
+			for student in student_list:
+				for metrics in self.metrics_list:
+					res = list(MasteryLevelStudent.objects.filter(student_id = student['student_id']).filter(**filter_lessons).values_list(metrics,flat=True))
+					res = self.set_default_metrics_data(res)
+					i_student.append(student['student_name'])
+					i_student.append(metrics)
+					if res:
+						i_student.extend(res)	
+					student_res_list.append(i_student)
+					i_student = []
+			return student_res_list
+		except Exception as e:
+			logger.error(e)
 
 	def set_default_metrics_data(self, data):
-		metrics_data = []
-		if not data:
-			[metrics_data.append(0) for i in self.content_list]
-		if len(data) != len(self.content_list):
-			[data.append(0) for i in range(len(self.content_list)-len(data))]
-			metrics_data = data
-		else:
-			metrics_data = data
-		return metrics_data
+		try:
+			metrics_data = []
+			if not data:
+				[metrics_data.append(0) for i in self.content_list]
+			if len(data) != len(self.content_list):
+				[data.append(0) for i in range(len(self.content_list)-len(data))]
+				metrics_data = data
+			else:
+				metrics_data = data
+			return metrics_data
+		except Exception as e:
+			logger.error(e)
 
 	def get_lesson_mastery_data(self):
 		data = {}
--- lesson/views.py
+++ lesson/views.py
@@ -4,43 +4,53 @@ import json
 # Create your views here.
 from account.views import construct_response
 
-def get_lessons(request):
-	if request.method == 'POST':
-		user = request.user
-		body_unicode = request.body.decode('utf-8')
-		data = json.loads(body_unicode)
-		parent_level = data.get('parentLevel', -2)
-		objSchoolDetails = LessonDetails(user, parent_level)
-		objSchoolData = objSchoolDetails.getPageData()
-		response = construct_response(0, '', '', objSchoolData);
-		response_text = json.dumps(response,ensure_ascii=False)
-		return HttpResponse(response_text,content_type='application/json')
-	else:
-		response = construct_response(1111,'wrong request','wrong request','')
-		response_text = json.dumps(response,ensure_ascii=False)
-		return HttpResponse(response_text,content_type='application/json')
+import logging
 
-def get_lesson_page_data(request):
-	if request.method == 'POST':
-		user = request.user
-		body_unicode = request.body.decode('utf-8')
-		data = json.loads(body_unicode)
-		lesson_id = data.get('lessonID')
-		startTimestamp = data.get('startTimestamp', 0)
-		endTimestamp = data.get('endTimestamp', 0)
-		if not lesson_id:
-			response = construct_response(2001, '', '', {});
+logger = logging.getLogger(__name__)
+
+def get_lessons(request):
+	try:	
+		if request.method == 'POST':
+			user = request.user
+			body_unicode = request.body.decode('utf-8')
+			data = json.loads(body_unicode)
+			parent_level = data.get('parentLevel', -2)
+			objSchoolDetails = LessonDetails(user, parent_level)
+			objSchoolData = objSchoolDetails.getPageData()
+			response = construct_response(0, '', '', objSchoolData);
 			response_text = json.dumps(response,ensure_ascii=False)
 			return HttpResponse(response_text,content_type='application/json')
-		objlesson_mastery = LessonMastery(lesson_id, startTimestamp, endTimestamp)
-		objlesson_mastery_data = objlesson_mastery.get_lesson_mastery_data()
-		response = construct_response(0, '', '', objlesson_mastery_data);
-		response_text = json.dumps(response,ensure_ascii=False)
-		return HttpResponse(response_text,content_type='application/json')
-	else:
-		response = construct_response(1111,'wrong request','wrong request','')
-		response_text = json.dumps(response,ensure_ascii=False)
-		return HttpResponse(response_text,content_type='application/json')
+		else:
+			response = construct_response(1111,'wrong request','wrong request','')
+			response_text = json.dumps(response,ensure_ascii=False)
+			return HttpResponse(response_text,content_type='application/json')
+	except Exception as e:
+		logger.error(e)
+
+def get_lesson_page_data(request):
+	try:
+		if request.method == 'POST':
+			user = request.user
+			body_unicode = request.body.decode('utf-8')
+			data = json.loads(body_unicode)
+			lesson_id = data.get('lessonID')
+			startTimestamp = data.get('startTimestamp', 0)
+			endTimestamp = data.get('endTimestamp', 0)
+			if not lesson_id:
+				response = construct_response(2001, '', '', {});
+				response_text = json.dumps(response,ensure_ascii=False)
+				return HttpResponse(response_text,content_type='application/json')
+			objlesson_mastery = LessonMastery(lesson_id, startTimestamp, endTimestamp)
+			objlesson_mastery_data = objlesson_mastery.get_lesson_mastery_data()
+			response = construct_response(0, '', '', objlesson_mastery_data);
+			response_text = json.dumps(response,ensure_ascii=False)
+			return HttpResponse(response_text,content_type='application/json')
+		else:
+			response = construct_response(1111,'wrong request','wrong request','')
+			response_text = json.dumps(response,ensure_ascii=False)
+			return HttpResponse(response_text,content_type='application/json')
+	except Exception as e:
+		logger.error(e)
 
 def get_lesson_mastery(request):
 	if request.method == 'GET':
