74b5ce3e7d5ec9fff1ca3663a2ffb62704041a81
Aishwarya Gaikwad <aishwarya.gaikwad@soft-corner.com>
2018-11-16 12:54:58 +0530

    functions for getting data and calculating result for aggregation are added

--- exam/exammetric.py
+++ exam/exammetric.py
@@ -4,12 +4,13 @@ from account.constants import *
 from exam.models import Exam, creation
 import datetime
 import json, time
+from itertools import *
 
 class Exam_details(BaseRoleAccess):
 	def __init__(self, user, parentLevel):
 		super(self.__class__, self).__init__(user, parentLevel)
 		self.parentLevelMethods = [self.getExamsData, self.getExamsData, self.getExamsData, self.getClassData]
-		self.totalstudent = {}
+		self.totalexams = {}
 		self.studentexamdata = {}
 
 	def convert_to_string(self, data):
@@ -21,7 +22,7 @@ class Exam_details(BaseRoleAccess):
 		Args:
 			None
 		Returns:
-			totalstudent(dict) : dict of schools with it's classes and students as a children
+			totalstudent(dict) : dict of schools with it's classes and exams as a children
 		"""
 		school_list = []
 		schools = UserInfoSchool.objects.filter(school_id__in = self.institutes)
@@ -30,49 +31,55 @@ class Exam_details(BaseRoleAccess):
 			school_info = {}
 			classes_in_school = list(UserInfoClass.objects.filter(parent=school.school_id).extra(select={'id':'class_id','name':'class_name'}).values('id','name'))
 			for classid in classes_in_school:
-				students_in_class = list(UserInfoStudent.objects.filter(parent=classid['id']).extra(select={'id':'student_id','name':'student_name'}).values('id','name'))
-				for student in students_in_class:
-					student['id'] = str(student['id'])
-				classid['children'] = students_in_class
+				exams_in_class = list(Exam.objects.filter(class_id=classid['id']).extra(select={'id':'exam_id','name':'exam_title'}).values('id','name'))
+				classid['children']={}
+				for exam in exams_in_class:
+					exam['id'] = str(exam['id'])
+					if exam['name'] not in classid['children'].keys():
+						classid['children'][exam['name']]= exam
+				classid['children']=list(map(lambda x: classid['children'][x],classid['children']))
 			school_info['id'] = str(school.school_id)
 			school_info['name'] = school.school_name
 			school_info['children'] = list(map(self.convert_to_string, classes_in_school))
 			school_list.append(school_info)
-		self.totalstudent["schools"] = school_list
-
-		return self.totalstudent
+		self.totalexams["schools"] = school_list
+		return self.totalexams
 	
 	def getClassData(self):
 		""" Used to get class data for user 
 		Args:
 			None
 		Returns:
-			totalstudent(dict) : returns student details with class/schools 
+			totalstudent(dict) : returns exam details with class/schools 
 		"""
 		class_info = {}
 		class_list = []
 		classes = list(UserInfoClass.objects.filter(class_id__in = self.classes).extra(select={'id':'class_id','name':'class_name'}).values('id','name'))
 		for classid in classes:
 			class_info = {}
-			students_in_class =list(UserInfoStudent.objects.filter(parent=classid['id']).extra(select={'id':'student_id','name':'student_name'}).values('id','name'))
-			for student in students_in_class:
-				student['id'] = str(student['id'])
-			class_info['children'] = students_in_class
+			exams_in_class = list(Exam.objects.filter(class_id=classid['id']).extra(select={'id':'exam_id','name':'exam_title'}).values('id','name'))
+			for exam in exams_in_class:
+				exam['id'] = str(exam['id'])
+			class_info['children'] = exams_in_class
 			class_info['id'] = str(classid['id'])
 			class_info['name'] = classid['name']
 			class_list.append(class_info)
-		self.totalstudent["schools"] = class_list
-		return self.totalstudent
+		self.totalexams["schools"] = class_list
+		print ('exams',totalexams)
+		return self.totalexams
+
 
 	def getPageData(self):
 		result = self.parentLevelMethods[self.role]()
 		return result	
 
 class Exammetric(object):
-	def __init__(self, user_id, start_time, end_time):
+	def __init__(self, exam_id, start_time, end_time):
 
-		self.user_id = user_id
-		self.exam_id = Exam.objects.filter(student_id = self.user_id).values('exam_id')
+		self.exam_id = exam_id
+		self.user_id = Exam.objects.filter(exam_id = self.exam_id).values('student_id')
+		self.class_id = Exam.objects.filter(exam_id=self.exam_id).values('class_id')[0]['class_id']
+		self.total_question = Exam.objects.filter(exam_id=self.exam_id).values('question_count')[0]['question_count']
 		self.startTimestamp = datetime.date.fromtimestamp(int(start_time)).strftime('%Y-%m-%d')
 		self.endTimestamp = datetime.date.fromtimestamp(int(end_time)).strftime('%Y-%m-%d')
 
@@ -83,8 +90,8 @@ class Exammetric(object):
 		Returns:
 			columns(list): It returns list of columns
 		"""
-		columns = [{'name':'first', 'title':'Exam Title'},{'name':'second', 'title':'Correct Questions'},
-					{'name':'third', 'title':'% Correct Questions'},{'name':'fourth', 'title':'Total Questions'}]
+		columns = [{'name':'first', 'title':'Student Name'},{'name':'second', 'title':'Correct Questions'},
+					{'name':'third', 'title':'% Correct Questions'}]
 	
 		return columns
 
@@ -95,40 +102,111 @@ class Exammetric(object):
 		Returns:
 			exam_metriclist(list):It returns student list including metrics result
 		"""
-		res1 = []
 		exam_metriclist = []
 		i_exam =[]
-		filter_exams= {}
-		filter_exams['date__range'] = (self.startTimestamp, self.endTimestamp)
-		student_list = Exam.objects.filter(student_id = self.user_id).filter(**filter_exams).values_list('student_id', 'exam_title', 'correct_questions' ,'question_count')
+		
+		total_question =Exam.objects.filter(exam_id=self.exam_id).values('question_count')[0]['question_count']
+		filter_students= {}
+		filter_students['date__range'] = (self.startTimestamp, self.endTimestamp)
+		filter_students['exam_id']=self.exam_id
+		count=0
+		student_list = UserInfoStudent.objects.filter(parent = self.class_id).values('student_id', 'student_name')		
 		for student in student_list:
-			i_exam = []
-			i_exam.append(student[1])
-			i_exam.append(student[2])
-			percent_correct_float = float(student[2]) / student[3] 
-			percent_correct = "{0:.2%}".format(percent_correct_float)
-			i_exam.append(percent_correct)
-			i_exam.append(student[3])
+			i_exam =[]
+			
+			i_exam.append(student['student_name'])
+			
+			filter_students['student_id']=student['student_id']
+
+			res = list(Exam.objects.filter(**filter_students).values_list('correct_questions'))
+			res1 = list(chain(*res))
+			print ("Result:", res1)
+			
+			if not res:
+				for i in range(2):
+					i_exam.append(0)
+			if res:
+				
+				correct = Exam.objects.filter(**filter_students).values('correct_questions')[0]['correct_questions']
+				i_exam.extend(res1)
+				percent_correct_float = float(correct) / total_question 
+				percent_correct = "{0:.2%}".format(percent_correct_float)		
+				i_exam.append(percent_correct)
+				
 			exam_metriclist.append(i_exam)
+				
 		return exam_metriclist
 
+	def aggregateresult(self, get_exam_metric, tc):
+		correct_sum = 0
+		count = 0
+		correct_percent = 0.0
+
+		aggregation = []
+		for record in get_exam_metric:
+			count+=1
+			correct_sum += record[1]
+			if(record[2] !=0):
+				percent = float(record[2].strip('%'))
+				correct_percent += (percent)
+
+
+		aggregation = [correct_sum,correct_percent,count,tc]
+		print("aggregateresult",aggregation)
+		result = self.getAggregationData(aggregation)
+		print('agg=====',result)
+		return result
+
+	def getAggregationData(self, aggregateresult):
+		# avg_correct = 0
+		aggregation = []
+		student_question = aggregateresult[2]
+		student_question *= aggregateresult[3]
+		student_percent = aggregateresult[2]
+		student_percent *= 100
+
+		avg_correct = aggregateresult[0]
+		avg_correct /= student_question
+		print('correct avg:',avg_correct)
+		avg_percent_correct = aggregateresult[1]
+		avg_percent_correct /=student_percent
+		print('correct % avg:',avg_percent_correct)
+		values = [str( "{0:.2}".format(avg_correct)),"{0:.2%}".format(avg_percent_correct)] 
+		average = {'name': 'Average', 'values': values}
+		aggregation.append(average)
+		return aggregation
+		
+
 	def getStudentData(self):
-		student_info = {}
-		student_data = list(creation.objects.filter(student_id = self.user_id).extra(select ={'exam_count':'exam_count', 'active_exam':'active_exam_count', 'complete_exam':'complete_exam_count'}).values('exam_count','active_exam','complete_exam'))
+		questionCount= {}
+		student_data = Exam.objects.filter(exam_id = self.exam_id).extra(select ={'question_count':'question_count'}).values('question_count')
 		for student in student_data:
-			student_info = {}
-			student_info['exam_count'] = student['exam_count']
-			student_info['active_exam'] = student['active_exam']
-			student_info['complete_exam'] = student['complete_exam']
-		return student_info
+			questionCount = {}
+			questionCount['question_count'] = student['question_count']
+		return questionCount
+
+	def getTopics(self):
+	
+		topics = Exam.objects.filter(exam_id = self.exam_id).values_list('question_sources')
+		# for topic in topics:
+		# 	topic_data =[]
+		# 	topic_data.append(topic['question_sources'])
+		topic_data = list(chain(*topics))
+		# print ('question_sources',topics)
+		# print ('gtjuhgsjdg46464   :',topic_data)
+		return topic_data
 
 	def get_exam_metric_data(self):
 		data = {}
 		columns = self.get_user_content()
 		rows = self.get_exam_metric()
+		average = self.aggregateresult(self.get_exam_metric(),self.total_question)
 		data['columns'] = columns
 		data['rows'] = rows
 		data['header'] = self.getStudentData()
+		data['topic'] = self.getTopics()
+		data['average']= average
+		#print('data:',data)
 		return data
 	
 
